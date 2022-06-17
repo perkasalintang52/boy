bcbefbaf3d5bb5a85874e63d9f1cff47059f693c
Tsukasa Tsukuyomi <107617869+TsukasaChann@users.noreply.github.com>
2022-06-18 04:32:03 +0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -28,7 +28,7 @@ module.exports = {
                 if (typeof user !== 'object') global.db.data.users[m.sender] = {}
                 if (user) {
                     if (!isNumber(user.exp)) user.exp = 0
-                    if (!isNumber(user.limit)) user.limit = 50
+                    if (!isNumber(user.limit)) user.limit = 100
                     if (!isNumber(user.joinlimit)) user.joinlimit = 1
                     if (!isNumber(user.money)) user.money = 100000
                     if (!isNumber(user.bank)) user.bank = 100000
