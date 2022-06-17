b0316730cf51ea6371a13b2bbffd1ad4ceafccdd
Tsukasa Tsukuyomi <107617869+TsukasaChann@users.noreply.github.com>
2022-06-18 01:24:37 +0700

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -35,26 +35,26 @@ let tags = {
 }
 const defaultMenu = {
   before: `
-┌─「 *{%me}* 」
-├ Hai, %name!
-├ Tersisa *%limit Limit*
-├ Role *%role*
-├ Level *%level (%exp / %maxexp)*
-├ [%xp4levelup]
-├ %totalexp XP secara Total
-│ 
-├ Tanggal: *%week %weton, %date*
-├ Tanggal Islam: *%dateIslamic*
-├ Waktu: *%time*
-│
-├ Uptime: *%uptime (%muptime)*
-├ Database: %rtotalreg dari %totalreg
-├ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
-└────
+┏━━〔 *TsukasaMD* 〕━━⬣
+┃⬡ Hai, %name!
+┃⬡ Tersisa *%limit Limit*
+┃⬡ Role *%role*
+┃⬡ Level *%level (%exp / %maxexp)*
+┃⬡ [%xp4levelup]
+┃⬡ %totalexp XP secara Total
+┃
+┃⬡ Tanggal: *%week %weton, %date*
+┃⬡ Tanggal Islam: *%dateIslamic*
+┃⬡ Waktu: *%time*
+┃
+┃⬡ Uptime: *%uptime (%muptime)*
+┃⬡ Database: %rtotalreg dari %totalreg
+┃⬡ Memory Used : *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB*
+┗━━━━━━⬣
 %readmore`.trimStart(),
-  header: ' *%category*',
-  body: ' • %cmd %islimit %isPremium',
-  footer: '\n',
+  header: '┏━━〔 %category 〕━⬣',
+  body: '┃⬡%cmd %islimit %isPremium',
+  footer: '┗━━⬣\n',
   after: `*Made by ♡*
 *%npmname* | %version
 ${'```%npmdesc```'}
