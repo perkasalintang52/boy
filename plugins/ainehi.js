a83b13383791f5a9d6aad2da37922ee454ac7cb3
Tsukasa Tsukuyomi <107617869+TsukasaChann@users.noreply.github.com>
2022-06-19 22:56:05 +0700

    Update ainehi.js

--- plugins/ainehi.js
+++ plugins/ainehi.js
@@ -1,13 +1,13 @@
 let fs = require('fs')
 const { MessageType } = require('@adiwajshing/baileys')
 let handler = async (m, { conn }) => {
-let helloaine = fs.readFileSync('./mp3/WhatsApp-Ptt-2021-07-14-at-18.12.33.opus') 
+let helloaine = fs.readFileSync('./mp3/PTT-20220619-WA0585.opus') 
 conn.sendFile(m.chat, helloaine, '', '', m, true)
 //conn.sendMessage(m.chat, helloaine, MessageType.audio, {quoted: m, mimetype: 'audio/mp4', ptt:true})
 // await conn.sendMessage(m.chat, { audio: { url: helloaine }, mimetype: 'audio/mp4'}, m)
 }
 
-handler.customPrefix = /^(hi|hii|hiii|hi aine|hii aine|hiii aine|hy|halo|hallo|helo|hello|hy aine|halo aine|hallo aine|helo aine|hello aine)$/i
+handler.customPrefix = /^(hi|hii|hiii|hi Tsukasa|hii tsukasa|hiii tsukasa|hy|halo|hallo|helo|hello|hy tsukasa|halo tsukasa|hallo tsukasa|helo tsukasa|hello tsukasa)$/i
 handler.command = new RegExp
 
 handler.limit = true
