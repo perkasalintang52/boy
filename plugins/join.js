fc25b8a846e87e490771cc3cd509ea8c8268b1aa
Tsukasa Tsukuyomi <107617869+TsukasaChann@users.noreply.github.com>
2022-06-21 01:20:23 +0700

    Update join.js

--- plugins/join.js
+++ plugins/join.js
@@ -3,7 +3,7 @@ let linkRegex = /chat.whatsapp.com\/([0-9A-Za-z]{20,24})( [0-9]{1,3})?/i
 let handler = async (m, { conn, text, isOwner, groupMetadata }) => {
     let [_, code, expired] = text.match(linkRegex) || []
     if (!code) throw 'Link invalid'
-    if (global.db.data.users[m.sender].joinlimit == 0) return m.reply('Maaf kamu sudah tidak bisa menggunakan free join..\nHarap hubungi *owner* kami')
+    if (global.db.data.users[m.sender].joinlimit == 999999) return m.reply('Maaf kamu sudah tidak bisa menggunakan free join..\nHarap hubungi *owner* kami')
     global.db.data.users[m.sender].joinlimit -= 999999
    // let id = m.chat
    // let groupMetadata = await conn.groupMetadata(m.chat)
